04ebe370706e283c3018228fd48c960c80ea2785
武流 岡田 <h1202019003@hiratagakuen.onmicrosoft.com>
2020-10-20 10:32:24 +0900

    sdssdd

--- gummy/gummy/ObjDpnatu.cpp
+++ gummy/gummy/ObjDpnatu.cpp
@@ -2,3 +2,17 @@
 #include"GameL/SceneObjManager.h"
 #include"ObjDonatu.h"
 #include"GameHead.h"//
+
+using namespace GameL;
+
+class CObjDonatu :public CObj
+{
+public:
+	CObjDonatu(){};
+	~CObjDonatu() {};
+	void Init();
+	void Action();
+	void Draw();
+
+private:
+};
