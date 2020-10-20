28ded0d47ed1653a29edd7be531eeb286e5b44e7
5040kouyou <sasii.504034@gmail.com>
2020-10-20 10:48:14 +0900

    1

---
+++ gummy/gummy/ObjBlock.cpp
@@ -0,0 +1,28 @@
+//使用ヘッダー
+#include"GameL/DrawTexture.h"
+#include"GameL/WinInputs.h"
+#include"GameL/SceneManager.h"
+
+#include"GameHead.h"
+#include"ObjBlock.h"
+
+//使用ネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjBlock::Init()
+{
+
+}
+
+//アクション
+void CObjBlock::Action()
+{
+
+}
+
+//ドロー
+void CObjBlock::Draw()
+{
+
+}
