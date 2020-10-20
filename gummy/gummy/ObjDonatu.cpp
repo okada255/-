d375649ae16ed39698dfc1120a51e43e4c7889f5
武流 岡田 <h1202019003@hiratagakuen.onmicrosoft.com>
2020-10-20 11:13:49 +0900

    ddd

--- gummy/gummy/ObjDonatu.cpp
+++ gummy/gummy/ObjDonatu.cpp
@@ -1,1 +1,22 @@
+#include"GameL/DrawTexture.h"
+#include"GameL/SceneObjManager.h"
 #include"ObjDonatu.h"
+#include"GameHead.h"//
+#include"GameL/WinInputs.h"
+
+using namespace GameL;
+
+void CObjDonatu::Init()
+{
+
+}
+
+void CObjDonatu::Action()
+{
+
+}
+
+void CObjDonatu::Draw()
+{
+
+}
