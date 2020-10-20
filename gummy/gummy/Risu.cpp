b628ae123c679ac0e4365af8b3763577a61c2d0c
壮一朗 生瀬 <h1202016018@hiratagakuen.onmicrosoft.com>
2020-10-20 10:52:43 +0900

    k

---
+++ gummy/gummy/Risu.cpp
@@ -0,0 +1,22 @@
+#include"GameL/DrawTexture.h"
+#include"GameL/SceneObjManager.h"
+#include"ObjRisu.h"
+#include"GameHead.h"//
+#include"GameL/WinInputs.h"
+
+using namespace GameL;
+
+void ObjRisu::Init()
+{
+
+}
+
+void ObjRisu::Action()
+{
+
+}
+
+void ObjRisu::Draw()
+{
+
+}
