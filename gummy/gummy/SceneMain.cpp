d292b520eacfaa8bcf7739793e44282822c5f4a6
武流 岡田 <h1202019003@hiratagakuen.onmicrosoft.com>
2020-10-22 15:39:16 +0900

    変更

--- gummy/gummy/SceneMain.cpp
+++ gummy/gummy/SceneMain.cpp
@@ -11,8 +11,13 @@ using namespace GameL;
 
 //使用ヘッダー
 #include"SceneMain.h"
-#include"ObjBlock.h"
 #include"GameL/GameHead.h"
+#include"ObjBlock.h"
+#include"ObjCookie.h"
+#include"ObjCandy.h"
+#include"ObjChoco.h"
+#include"ObjDonatu.h"
+#include"ObjGummy.h"
 
 
 //コンストラクタ
@@ -35,9 +40,25 @@ void CSceneMain::InitScene()
 	CObjBlock* objb = new CObjBlock();
 	Objs::InsertObj(objb, OBJ_BLOCK, 9);
 
-	//Blockオブジェクト作成
+	//Candyオブジェクト作成
 	CObjCandy* objb = new CObjCandy();
 	Objs::InsertObj(objb, OBJ_CANDY, 9);
+
+	//Cookieオブジェクト作成
+	CObjCookie* objb = new CObjCookie();
+	Objs::InsertObj(objb, OBJ_COOKIE, 9);
+
+	//Donatuオブジェクト作成
+	CObjDonatu* objb = new CObjDonatu();
+	Objs::InsertObj(objb, OBJ_DONATU, 9);
+
+	//Chocoオブジェクト作成
+	CObjChoco* objb = new CObjChoco();
+	Objs::InsertObj(objb, OBJ_CANDY, 9);
+
+	//Gummyオブジェクト作成
+	CObjGummy* objb = new CObjGummy();
+	Objs::InsertObj(objb, OBJ_GUMMY, 9);
 }
 
 //実行中メソッド
