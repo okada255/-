df8b9cbfe7cf99fc7e94b5bee17a6b77efaac0c6
武流 岡田 <h1202019003@hiratagakuen.onmicrosoft.com>
2020-10-28 13:33:06 +0900

    henkou

---
+++ gummy/gummy/ObjTime.cpp
@@ -0,0 +1,17 @@
+//使用するヘッダーファイル
+#include"GameL/DrawTexture.h"
+#include"GameL/SceneManager.h"
+#include"GameL/WinInputs.h"
+#include"GameHead.h"
+#include"ObjTime.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void 
+
+//アクション
+
+//ドロー
+
