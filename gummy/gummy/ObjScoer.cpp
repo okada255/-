49b7885fe7054097e589295168bad254260ff500
武流 岡田 <h1202019003@hiratagakuen.onmicrosoft.com>
2020-10-29 15:02:43 +0900

    変更

---
+++ gummy/gummy/ObjScoer.cpp
@@ -0,0 +1,7 @@
+//使用するヘッダーファイル
+#include"GameL/DrawTexture.h"
+#include"GameL/SceneManager.h"
+#include"GameL/WinInputs.h"
+#include"GameHead.h"
+#include"ObjScore.h"
+#include"GameL/DrawFont.h"
--- gummy/gummy/ObjTitle.cpp
+++ gummy/gummy/ObjTitle.cpp
@@ -30,7 +30,18 @@ void CObjTitle::Draw() {
 	float c[4] = { 1.0f,1.0f,1.0f,1.0f };
 	Font::StrDraw(L"GUMMY", 220, 200, 150, c);
 
+	//ランキング
+	Font::StrDraw(L"ランキング",600,0,24,c);
+	
+	for(int i = 0;i<15;i++)
+	{
+		wchar_t str[256];
+		swprintf_s(str,L"%2d位%12d位点",i+1,(i+1)*100);
+		Font::StrDraw(str,670,24+24*i,12,c);
+	}
+
 	Font::StrDraw(L"Game Start ", 320, 380, 32, c);
 	Font::StrDraw(L" Push [Enter]Key ", 260, 430, 32, c);
 
+	Font::StrDraw(L"★ClickReset", 600, 380, 16, c);
 }
